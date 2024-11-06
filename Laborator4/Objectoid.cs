1994d22e06de082faa6800c74495c8987674cd7a
DicaIonela <ionela.dica@student.usv.ro>
2024-11-06 20:50:51 +0200

    Laborator4

---
+++ Laborator4/Objectoid.cs
@@ -0,0 +1,97 @@
+﻿//MINUT 16:22
+
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator4
+{
+    class Objectoid
+    {
+        private bool visibility;
+        private bool isGravityBound;
+        private Color colour;
+        private List<Vector3> coordList;
+        private Randomizer rando;
+
+        private int GRAVITY_OFFSET = 1;
+        public Objectoid(bool gravity_status)
+        {
+            visibility = true;
+            rando = new Randomizer();
+            colour = rando.RandomColor();
+            coordList = new List<Vector3>();
+            isGravityBound = gravity_status;
+            int size_offset = rando.RandomInt(3,7);
+            int height_offset = rando.RandomInt(20, 100);
+            int radial_offset = rando.RandomInt(3, 20);
+
+            coordList.Add(new Vector3(0* size_offset+radial_offset, 0 * size_offset+ height_offset, 1 * size_offset + radial_offset));
+            coordList.Add(new Vector3(0 * size_offset + radial_offset, 0 * size_offset + height_offset, 0 * size_offset + radial_offset));
+            coordList.Add(new Vector3(1 * size_offset + radial_offset, 0 * size_offset + height_offset, 1 * size_offset + radial_offset));
+            coordList.Add(new Vector3(1 * size_offset + radial_offset, 0 * size_offset + height_offset, 0 * size_offset + radial_offset));
+            coordList.Add(new Vector3(1 * size_offset + radial_offset, 1 * size_offset + height_offset, 1 * size_offset + radial_offset));
+            coordList.Add(new Vector3(1 * size_offset + radial_offset, 1 * size_offset + height_offset, 0 * size_offset + radial_offset));
+            coordList.Add(new Vector3(0 * size_offset + radial_offset, 1 * size_offset + height_offset, 1 * size_offset + radial_offset));
+            coordList.Add(new Vector3(0 * size_offset + radial_offset, 1 * size_offset + height_offset, 0 * size_offset + radial_offset));
+            coordList.Add(new Vector3(0 * size_offset + radial_offset, 0 * size_offset + height_offset, 1 * size_offset + radial_offset));
+            coordList.Add(new Vector3(0 * size_offset + radial_offset, 0 * size_offset + height_offset, 0 * size_offset + radial_offset));
+        }
+        public void UpdatePosition(bool gravity_status)
+        {
+            if (visibility&&gravity_status&&!GroundCollisionDetected())
+            {
+                for(int i=0;i<coordList.Count;i++)
+                {
+                    coordList[i]=new Vector3(coordList[i].X, coordList[i].Y-GRAVITY_OFFSET, coordList[i].Z);
+                }
+            }
+        }
+        public bool GroundCollisionDetected()
+        {
+            foreach (Vector3 v in coordList)
+            {
+                if (v.Y<=0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Draw()
+        {
+            if (visibility)
+            {
+                GL.Color3(colour);
+                GL.Begin(PrimitiveType.QuadStrip);
+                foreach (Vector3 v in coordList)
+                {
+                    GL.Vertex3(v);
+                }
+                GL.End();
+            }
+        }
+        public void ToggleVisibility()
+        {
+            visibility = !visibility;
+        }
+        //public void ToggleGravity()
+        //{
+        //    isGravityBound = !isGravityBound;
+        //}
+        //public void SetGravity()
+        //{
+        //    isGravityBound = true;
+        //}
+        //public void UnsetGravity()
+        //{
+        //    isGravityBound = false;
+        //}
+
+    }
+}
---
+++ Laborator4/Window3D.cs
@@ -0,0 +1,202 @@
+﻿//Min 18.24
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Laborator4
+{
+    class Window3D : GameWindow
+    {
+
+        private KeyboardState previousKeyboard;
+        private MouseState previousMouse;
+        private readonly Randomizer rando;
+        //private Triangle3D firstTriangle;
+        private readonly Axes ax;
+        private readonly Grid grid;
+        private readonly Camera3DIsometric cam;
+        private Objectoid obj;
+        private List<Objectoid> rainOfObjects;
+
+        private bool GRAVITY = true;
+
+        // CONST
+        private Color DEFAULT_BKG_COLOR = Color.FromArgb(49,50,51);
+
+
+        public Window3D() : base(1280, 768, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+
+            rando = new Randomizer();
+            ax = new Axes();
+            grid = new Grid();
+            cam= new Camera3DIsometric();
+            rainOfObjects= new List<Objectoid>();
+
+            DisplayHelp();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            // setare fundal
+            GL.ClearColor(DEFAULT_BKG_COLOR);
+
+            // setare viewport
+            GL.Viewport(0, 0, this.Width, this.Height);
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)this.Width / (float)this.Height, 1, 512);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+            cam.SetCamera();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+
+            // LOGIC CODE
+            KeyboardState currentKeyboard = Keyboard.GetState();
+            MouseState currentMouse = Mouse.GetState();
+
+            if (currentKeyboard[Key.Escape])
+            {
+                Exit();
+            }
+
+            if (currentKeyboard[Key.H] && !previousKeyboard[Key.H])
+            {
+                DisplayHelp();
+            }
+
+            if (currentKeyboard[Key.R] && !previousKeyboard[Key.R])
+            {
+                GL.ClearColor(DEFAULT_BKG_COLOR);
+                ax.Show();
+                grid.Show();
+            }
+
+            if (currentKeyboard[Key.K] && !previousKeyboard[Key.K])
+            {
+                ax.ToggleVisibility();
+            }
+
+            // triangle manipulation
+            if (currentKeyboard[Key.B] && !previousKeyboard[Key.B])
+            {
+                GL.ClearColor(rando.RandomColor());
+            }
+
+            if (currentKeyboard[Key.V] && !previousKeyboard[Key.V])
+            {
+                grid.ToggleVisibility();
+            }
+            if (currentKeyboard[Key.G] && !previousKeyboard[Key.G])
+            {
+                //foreach (Objectoid obj in rainOfObjects)
+                //{
+                //    obj.ToggleGravity();
+                //}
+                GRAVITY = !GRAVITY;
+            }
+
+            //camera control isometric mode
+            if (currentKeyboard[Key.W])
+            {
+                cam.MoveForward();
+            }
+
+            if (currentKeyboard[Key.S])
+            {
+                cam.MoveBackward();
+            }
+
+            if (currentKeyboard[Key.A])
+            {
+                cam.MoveLeft();
+            }
+            if (currentKeyboard[Key.D])
+            {
+                cam.MoveRight();
+            }
+            if (currentKeyboard[Key.Q])
+            {
+                cam.MoveUp();
+            }
+            if (currentKeyboard[Key.E])
+            {
+                cam.MoveDown();
+            }
+            if (currentMouse[MouseButton.Left] && previousMouse[MouseButton.Left])
+            {
+                rainOfObjects.Add(new Objectoid(GRAVITY));
+            }
+            if (currentMouse[MouseButton.Right] && previousMouse[MouseButton.Right])
+            {
+                rainOfObjects.Clear();
+            }
+            foreach (Objectoid obj in rainOfObjects)
+            {
+                obj.UpdatePosition(GRAVITY);
+            }
+
+            previousKeyboard = currentKeyboard;
+            previousMouse= currentMouse;
+            // END logic code
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            // RENDER CODE
+            grid.Draw();
+            ax.Draw();
+            //Objectoid obj = new Objectoid();
+            //obj.Draw();
+            foreach(Objectoid obj in rainOfObjects)
+            {
+                obj.Draw();
+            }
+
+            // END render code
+            SwapBuffers();
+
+        }
+
+        private void DisplayHelp()
+        {
+            Console.WriteLine("\n     MENU");
+            Console.WriteLine(" H - meniu");
+            Console.WriteLine(" ESC - parasire aplicatie");
+            Console.WriteLine(" R - reseteaza scena la valori implicite");
+            Console.WriteLine(" B - schimbare culoare de fundal");
+            Console.WriteLine(" V - schimbare vizibilitate grid");
+            Console.WriteLine(" W,A,S,D,Q,E - deplasare camera(isometric)");
+            Console.WriteLine(" K - schimbare vizibilitate sistem de axe");
+            Console.WriteLine(" Mouse clic stanga - genereaza un nou obiect la o inatime aleatoare");
+            Console.WriteLine(" Mouse clic dreapta - curata lista de obiecte");
+            Console.WriteLine(" G - manipuleaza gravitatia");
+        }
+
+    }
+}
