88da1b40b6392ee5c5fd4a3b6230dc5d19fd96d4
DicaIonela <ionela.dica@student.usv.ro>
2024-11-07 21:22:56 +0200

    Laborator05

---
+++ Laborator05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Laborator05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window3D ex = new Window3D();
+            ex.Run(30.0, 0.0);
+        }
+    }
+}
